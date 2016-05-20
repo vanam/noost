aec64904e83e77b1c2b025e66f056bdf0457170e
Sinner <hallowedbythyname@seznam.cz>
2016-05-20 15:37:20 +0200

    Setup virtual host.

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ Simple bootstrapping tool for PHP web development using modern tools.
 
 * [Composer](https://getcomposer.org/)
 
-
 ## Why is it named Noost?
 
 Name come from words `Nette` and `boost` since it is tool primary designed for Nette framework.
+
+## Roadmap
+
+* Initiate project from custom composer package
+* Define custom path to apache configuration file
+* Multiple operating systems
+* Multiple frontend projects (eg. front and admin)
--- noost.sh
+++ noost.sh
@@ -11,6 +11,8 @@ NETTE_PROJECT=0
 ################################################################################
 
 PROJECT_PATH=''
+FULL_PROJECT_PATH=''
+PROJECT_NAME=''
 
 ################################################################################
 # HELPERS                                                                      #
@@ -18,15 +20,19 @@ PROJECT_PATH=''
 
 function check_command {
     command_chunks=( $1 )
-    $1 >/dev/null 2>&1 || { echo >&2 "Tool '${command_chunks[0]}' required, but it's not installed. Aborting."; exit 1; }
+    $1 >/dev/null 2>&1 || { errecho "Tool '${command_chunks[0]}' required, but it's not installed. Aborting."; exit 1; }
 }
 
 function check_tools {
     check_command "composer -V"
 }
 
+function errecho {
+    echo >&2 "$1"
+}
+
 function not_implemented_yet {
-    echo "'$1' not implented yet."
+    echo >&2 "'$1' not implented yet."
 }
 
 function print_help {
@@ -34,10 +40,9 @@ function print_help {
     echo "=================================="
     echo ""
     echo ""
-    echo "Usage: $1 <> [options]"
+    echo "Usage: $1 [options] <project_path> <project_name>"
     echo "Available options"
-
-    echo "    -c                         delete all"
+    echo ""
     echo "    -n                         creates Standard Nette Web Project"
     echo "    -h                         displays help"
 }
@@ -47,16 +52,18 @@ function print_help {
 ################################################################################
 
 function create_frontend_folder_structure {
-    not_implemented_yet "create_frontend_folder_structure"
+    # TODO Invesigate why shellcheck displays warning on the next line
+    mkdir -p "$FULL_PROJECT_PATH/www/"{"src/"{"font","image","js","style"},"dist/"{"font","image","style","js"}}
 }
 
 function init_nette_project {
     echo "Creating Standard Nette Web Project..."
     echo "--------------------------------------"
-    echo "Location: '$PROJECT_PATH'"
+    echo "Location: '$FULL_PROJECT_PATH'"
     echo ""
-    composer create-project nette/web-project "$PROJECT_PATH"
-    # TODO make log/temp folders writable
+    composer create-project nette/web-project "$FULL_PROJECT_PATH"
+    # Make log/temp folders writable
+    chmod -R a+rw "$FULL_PROJECT_PATH/temp" "$FULL_PROJECT_PATH/log"
 }
 
 function init_plain_project {
@@ -64,7 +71,35 @@ function init_plain_project {
     echo "-----------------------------"
     echo "Location: '$PROJECT_PATH'"
     echo ""
-    mkdir "$PROJECT_PATH"
+}
+
+function setup_virtual_host {
+    # Add line 127.0.1.1 project.l into /etc/hosts
+    echo "127.0.1.1 $PROJECT_NAME.l" | sudo tee -a /etc/hosts > /dev/null
+    # Create entry in server config /etc/apache2/extra/httpd-vhosts.conf
+    VIRTUAL_HOST="
+        <VirtualHost *:80>
+            ServerName ${PROJECT_NAME}.l
+            ServerAdmin webmaster@localhost
+
+            DocumentRoot ${FULL_PROJECT_PATH}/www
+            <Directory />
+                Options FollowSymLinks
+                AllowOverride All
+            </Directory>
+            <Directory ${FULL_PROJECT_PATH}/www>
+                Options Indexes FollowSymLinks MultiViews
+                AllowOverride All
+                Require all granted
+            </Directory>
+            ErrorLog ${FULL_PROJECT_PATH}/log/error.log
+            LogLevel warn
+            CustomLog ${FULL_PROJECT_PATH}/log.log combined
+        </VirtualHost>"
+    sudo tee -a /etc/apache2/sites-enabled/localhost-site.conf <<< "$VIRTUAL_HOST" > /dev/null
+
+    # Restart apache
+    sudo service apache2 restart
 }
 
 ################################################################################
@@ -78,35 +113,55 @@ check_tools
 # Load arguments #
 ##################
 
-# First argument is a project path
-if [ -z "$1" ]
-then
-    echo "No project path provided. Aborting."
-    exit 1
-fi
-PROJECT_PATH="$1"
-
-# Check if project path doesn't collide with existing content
-if [ -a "$PROJECT_PATH" ]
-then
-    echo "Provided path collide with existing file/folder. Aborting."
-    exit 1
-fi
-shift
-
 # Read options
 while [ "$1" != "" ]
 do
     case $1 in
+        -h) shift
+            print_help $0
+            exit 0
+            ;;
+
         -n) shift
             NETTE_PROJECT=1
             ;;
 
-        *)  echo "Skipping unknown option '$1'."
-            shift
+        *)  break                       # First unknown option is a project path
             ;;
     esac
 done
+
+# Load project path
+if [ -z "$1" ]
+then
+    errecho "No project path provided. Aborting."
+    echo ""
+    print_help $0
+    exit 1
+fi
+PROJECT_PATH="$1"
+
+# Load project name
+if [ -z "$2" ]
+then
+    errecho "No project name provided. Aborting."
+    echo ""
+    print_help $0
+    exit 1
+fi
+# TODO validate project name (only [a-z]+)
+PROJECT_NAME="$2"
+
+# Check if project path doesn't collide with existing content
+if [ -a "$PROJECT_PATH" ]
+then
+    errecho "Provided path collide with existing file/folder. Aborting."
+    exit 1
+fi
+
+# Create project folder and determine FULL_PROJECT_PATH
+mkdir -p "$PROJECT_PATH"
+FULL_PROJECT_PATH=`cd "$PROJECT_PATH"; pwd`
 
 #############
 # Bootstrap #
@@ -122,3 +177,6 @@ fi
 
 # Create frontend folder structure
 create_frontend_folder_structure
+
+# Setup virtual host
+setup_virtual_host
